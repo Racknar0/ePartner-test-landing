4599c5882c823e6e59ce5027f03d1c06f74cccf4
Racknar0 <racknarow1@gmail.com>
2022-05-04 02:01:59 -0500

    update

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+      });
+});
+
+
